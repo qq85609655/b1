07b1c4f33b98f956b47485ee370767be82828cf3
Administrator <253479240@QQ.COM>
2018-08-13 11:11:31 +0800

    4444灌灌灌灌灌过过过过过

--- src/main/java/com/gtafe/data/center/dataetl/schedule/RunProduceSchecule.java
+++ src/main/java/com/gtafe/data/center/dataetl/schedule/RunProduceSchecule.java
@@ -36,48 +36,51 @@ public class RunProduceSchecule {
         dateFormatterChina.setTimeZone(timeZoneChina);//设置系统时区
         String produceNames = PropertyUtils.getProperty("config.properties", "produceNames");
         String produceDbLink = PropertyUtils.getProperty("config.properties", "produceDbLink");
+        String RunProduceFlag = PropertyUtils.getProperty("config.properties", "RunProduceFlag");
         System.out.println(produceNames);
         System.out.println(produceDbLink);
-        if (StringUtil.isNotBlank(produceNames) && StringUtil.isNotBlank(produceDbLink)) {
-            String[] prodstr = produceNames.split(";");
-            DatasourceVO datasourceVO = mapper.queryDatasourceInfoById(Integer.parseInt(produceDbLink));
-            ConnectDB connectDB = StringUtil.getEntityBy(datasourceVO);
-            Connection connection = null;
-            Date nowTime = new Date(System.currentTimeMillis());
-            System.out.println(System.currentTimeMillis());
-            SimpleDateFormat sdFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:dd");
-            String retStrFormatNowDate = sdFormatter.format(nowTime);
-            System.out.println(retStrFormatNowDate);
-            connection = null;
-            try {
-                for (String prd : prodstr) {
-                    long s = System.currentTimeMillis();
-                    System.out.println(" 开始执行存储过程：" + prd);
-                    connection = connectDB.getConn();
+        if (StringUtil.isNotBlank(RunProduceFlag) && RunProduceFlag.equals("Y")) {
+            if (StringUtil.isNotBlank(produceNames) && StringUtil.isNotBlank(produceDbLink)) {
+                String[] prodstr = produceNames.split(";");
+                DatasourceVO datasourceVO = mapper.queryDatasourceInfoById(Integer.parseInt(produceDbLink));
+                ConnectDB connectDB = StringUtil.getEntityBy(datasourceVO);
+                Connection connection = null;
+                Date nowTime = new Date(System.currentTimeMillis());
+                System.out.println(System.currentTimeMillis());
+                SimpleDateFormat sdFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:dd");
+                String retStrFormatNowDate = sdFormatter.format(nowTime);
+                System.out.println(retStrFormatNowDate);
+                connection = null;
+                try {
+                    for (String prd : prodstr) {
+                        long s = System.currentTimeMillis();
+                        System.out.println(" 开始执行存储过程：" + prd);
+                        connection = connectDB.getConn();
+                        if (connection != null) {
+                            CallableStatement cs = connection.prepareCall("{call " + prd + "}");
+                            cs.execute();
+                            System.out.println("执行结束了");
+                            long s2 = System.currentTimeMillis();
+                            System.out.println("耗时：" + (s2 - s) / 1000 % 60 + " 秒");       //执行
+                            cs.close();
+                            connection.close();
+                        } else {
+                            System.out.println("数据库连接不上啊!");
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
                     if (connection != null) {
-                        CallableStatement cs = connection.prepareCall("{call " + prd + "}");
-                        cs.execute();
-                        System.out.println("执行结束了");
-                        long s2 = System.currentTimeMillis();
-                        System.out.println("耗时：" + (s2 - s) / 1000 % 60 + " 秒");       //执行
-                        cs.close();
-                        connection.close();
-                    } else {
-                        System.out.println("数据库连接不上啊!");
+                        try {
+                            connection.close();
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                        }
                     }
                 }
-            } catch (Exception e) {
-                e.printStackTrace();
-            } finally {
-                if (connection != null) {
-                    try {
-                        connection.close();
-                    } catch (SQLException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
 
+            }
         }
     }
 
